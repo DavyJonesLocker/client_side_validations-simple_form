598761196b3b008ae19ab032c3afb90b306e9770
Brian Cardarella <bcardarella@gmail.com>
2012-01-24 05:20:01 -0500

    Regenerated JavaScript

--- vendor/assets/javascripts/rails.validations.simple_form.js
+++ vendor/assets/javascripts/rails.validations.simple_form.js
@@ -1,28 +1,34 @@
-/*!
- * Client Side Validations - SimpleForm - v2.0.0.beta.2
- * https://github.com/dockyard/client_side_validations-simple_form
- *
- * Copyright (c) 2012 DockYard, LLC
- * Licensed under the MIT license
- * http://www.opensource.org/licenses/mit-license.php
- */
 
-ClientSideValidations.formBuilders['SimpleForm::FormBuilder'] = {
-  add: function(element, settings, message) {
-    if (element.data('valid') !== false) {
-      var wrapper = element.closest(settings.wrapper_tag);
-      wrapper.addClass(settings.wrapper_error_class);
-      var errorElement = $('<' + settings.error_tag + ' class="' + settings.error_class + '">' + message + '</' + settings.error_tag + '>');
-      wrapper.append(errorElement);
-    } else {
-      element.parent().find(settings.error_tag + '.' + settings.error_class).text(message);
+/*
+  Client Side Validations - SimpleForm - v1.5.0.beta.2
+  https://github.com/dockyard/client_side_validations-simple_form
+
+  Copyright (c) 2012 DockYard, LLC
+  Licensed under the MIT license
+  http://www.opensource.org/licenses/mit-license.php
+*/
+
+(function() {
+
+  ClientSideValidations.formBuilders['SimpleForm::FormBuilder'] = {
+    add: function(element, settings, message) {
+      var errorElement, wrapper;
+      if (element.data('valid') !== false) {
+        wrapper = element.closest(settings.wrapper_tag);
+        wrapper.addClass(settings.wrapper_error_class);
+        errorElement = $('<' + settings.error_tag + ' class="' + settings.error_class + '">' + message + '</' + settings.error_tag + '>');
+        return wrapper.append(errorElement);
+      } else {
+        return element.parent().find(settings.error_tag + '.' + settings.error_class).text(message);
+      }
+    },
+    remove: function(element, settings) {
+      var errorElement, wrapper;
+      wrapper = element.closest(settings.wrapper_tag + '.' + settings.wrapper_error_class);
+      wrapper.removeClass(settings.wrapper_error_class);
+      errorElement = wrapper.find(settings.error_tag + '.' + settings.error_class);
+      return errorElement.remove();
     }
-  },
-  remove: function(element, settings) {
-    var wrapper = element.closest(settings.wrapper_tag + '.' + settings.wrapper_error_class);
-    wrapper.removeClass(settings.wrapper_error_class);
-    var errorElement = wrapper.find(settings.error_tag + '.' + settings.error_class);
-    errorElement.remove();
-  }
-}
+  };
 
+}).call(this);
