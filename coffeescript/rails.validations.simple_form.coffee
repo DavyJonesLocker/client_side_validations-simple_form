3b2ee29b82f762aa629900d718c23cf9ae64bd2c
Brian Cardarella <bcardarella@gmail.com>
2012-01-24 06:59:06 -0500

    More Coffeescript goodness

--- coffeescript/rails.validations.simple_form.coffee
+++ coffeescript/rails.validations.simple_form.coffee
@@ -12,14 +12,14 @@ ClientSideValidations.formBuilders['SimpleForm::FormBuilder'] =
     if element.data('valid') != false
       wrapper = element.closest(settings.wrapper_tag)
       wrapper.addClass(settings.wrapper_error_class)
-      errorElement = $('<' + settings.error_tag + ' class="' + settings.error_class + '">' + message + '</' + settings.error_tag + '>')
+      errorElement = $("<#{settings.error_tag}/>", { class: settings.error_class, text: message })
       wrapper.append(errorElement)
     else
-      element.parent().find(settings.error_tag + '.' + settings.error_class).text(message)
+      element.parent().find("#{settings.error_tag}.#{settings.error_class}").text(message)
 
   remove: (element, settings) ->
-    wrapper = element.closest(settings.wrapper_tag + '.' + settings.wrapper_error_class)
+    wrapper = element.closest("#{settings.wrapper_tag}.#{settings.wrapper_error_class}")
     wrapper.removeClass(settings.wrapper_error_class)
-    errorElement = wrapper.find(settings.error_tag + '.' + settings.error_class)
+    errorElement = wrapper.find("#{settings.error_tag}.#{settings.error_class}")
     errorElement.remove()
 
--- vendor/assets/javascripts/rails.validations.simple_form.js
+++ vendor/assets/javascripts/rails.validations.simple_form.js
@@ -16,17 +16,20 @@
       if (element.data('valid') !== false) {
         wrapper = element.closest(settings.wrapper_tag);
         wrapper.addClass(settings.wrapper_error_class);
-        errorElement = $('<' + settings.error_tag + ' class="' + settings.error_class + '">' + message + '</' + settings.error_tag + '>');
+        errorElement = $("<" + settings.error_tag + "/>", {
+          "class": settings.error_class,
+          text: message
+        });
         return wrapper.append(errorElement);
       } else {
-        return element.parent().find(settings.error_tag + '.' + settings.error_class).text(message);
+        return element.parent().find("" + settings.error_tag + "." + settings.error_class).text(message);
       }
     },
     remove: function(element, settings) {
       var errorElement, wrapper;
-      wrapper = element.closest(settings.wrapper_tag + '.' + settings.wrapper_error_class);
+      wrapper = element.closest("" + settings.wrapper_tag + "." + settings.wrapper_error_class);
       wrapper.removeClass(settings.wrapper_error_class);
-      errorElement = wrapper.find(settings.error_tag + '.' + settings.error_class);
+      errorElement = wrapper.find("" + settings.error_tag + "." + settings.error_class);
       return errorElement.remove();
     }
   };
