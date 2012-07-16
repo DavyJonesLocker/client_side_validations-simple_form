97f75d99cbb0efeb2a4909feed5e750dbe74d25a
Brian Cardarella <bcardarella@gmail.com>
2012-07-16 19:58:28 -0400

    Regenerated JavaScript

--- vendor/assets/javascripts/rails.validations.simple_form.js
+++ vendor/assets/javascripts/rails.validations.simple_form.js
@@ -13,25 +13,37 @@
 
   ClientSideValidations.formBuilders['SimpleForm::FormBuilder'] = {
     add: function(element, settings, message) {
-      var errorElement, wrapper;
-      if (element.data('valid') !== false) {
-        wrapper = element.closest(settings.wrapper_tag);
-        wrapper.addClass(settings.wrapper_error_class);
-        errorElement = $("<" + settings.error_tag + "/>", {
-          "class": settings.error_class,
-          text: message
-        });
-        return wrapper.append(errorElement);
-      } else {
-        return element.parent().find("" + settings.error_tag + "." + settings.error_class).text(message);
-      }
+      debugger;
     },
-    remove: function(element, settings) {
-      var errorElement, wrapper;
-      wrapper = element.closest("" + settings.wrapper_tag + "." + settings.wrapper_error_class);
-      wrapper.removeClass(settings.wrapper_error_class);
-      errorElement = wrapper.find("" + settings.error_tag + "." + settings.error_class);
-      return errorElement.remove();
+    remove: function(element, settings) {},
+    wrappers: {
+      "default": {
+        add: function(element, settings, message) {
+          var errorElement, wrapper;
+          if (element.data('valid') !== false) {
+            wrapper = element.closest(settings.wrapper_tag);
+            wrapper.addClass(settings.wrapper_error_class);
+            errorElement = $("<" + settings.error_tag + "/>", {
+              "class": settings.error_class,
+              text: message
+            });
+            return wrapper.append(errorElement);
+          } else {
+            return element.parent().find("" + settings.error_tag + "." + settings.error_class).text(message);
+          }
+        },
+        remove: function(element, settings) {
+          var errorElement, wrapper;
+          wrapper = element.closest("" + settings.wrapper_tag + "." + settings.wrapper_error_class);
+          wrapper.removeClass(settings.wrapper_error_class);
+          errorElement = wrapper.find("" + settings.error_tag + "." + settings.error_class);
+          return errorElement.remove();
+        }
+      },
+      bootstrap: {
+        add: function(element, settings, message) {},
+        remove: function(element, settings) {}
+      }
     }
   };
 
