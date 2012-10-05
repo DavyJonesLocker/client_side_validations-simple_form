5e1158a36e5c4944e1518c6836abc6065cc2b585
Brian Cardarella <bcardarella@gmail.com>
2012-10-05 17:18:49 -0400

    Regenerated JavaScript

--- vendor/assets/javascripts/rails.validations.simple_form.js
+++ vendor/assets/javascripts/rails.validations.simple_form.js
@@ -1,6 +1,6 @@
 
 /*
-  Client Side Validations - SimpleForm - v2.0.0.beta.2
+  Client Side Validations - SimpleForm - v2.0.0.rc.1
   https://github.com/dockyard/client_side_validations-simple_form
 
   Copyright (c) 2012 DockYard, LLC
