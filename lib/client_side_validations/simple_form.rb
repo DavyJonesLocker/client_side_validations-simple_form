6346980c92b1a69849607e58ffac4981bae76312
Brian Cardarella <bcardarella@gmail.com>
2012-10-07 20:45:53 -0400

    Ensure ClientSideValidations module exists

--- lib/client_side_validations/simple_form.rb
+++ lib/client_side_validations/simple_form.rb
@@ -1,3 +1,5 @@
+module ClientSideValidations; end
+
 require 'simple_form'
 require 'client_side_validations/simple_form/form_builder'
 require 'client_side_validations/simple_form/engine'
--- test/base_helper.rb
+++ test/base_helper.rb
@@ -9,8 +9,6 @@ if RUBY_VERSION >= '1.9.3'
   require 'debugger'
 end
 
-module ClientSideValidations; end
-
 require 'rails/engine'
 
 module TestApp
