9474deb51b3040bb4d9786a8dd2bca9fd2711ad1
Geremia Taglialatela <tagliala.dev@gmail.com>
2017-01-22 16:16:10 +0100

    Update syntax to match client_side_validations

--- lib/client_side_validations/simple_form.rb
+++ lib/client_side_validations/simple_form.rb
@@ -1,8 +1,8 @@
 require 'simple_form'
-
 require 'client_side_validations'
-
 require 'client_side_validations/simple_form/form_builder'
-require 'client_side_validations/simple_form/engine'
 
-require 'client_side_validations/generators/simple_form' if defined?(::Rails)
+if defined?(::Rails)
+  require 'client_side_validations/simple_form/engine'
+  require 'client_side_validations/generators/simple_form'
+end
