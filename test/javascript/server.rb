803ff19b9326dfac0e9abef04de69a19fb51d065
Geremia Taglialatela <tagliala.dev@gmail.com>
2018-09-08 17:32:28 +0200

    Update QUnit to 2.6.2

--- test/javascript/server.rb
+++ test/javascript/server.rb
@@ -35,7 +35,7 @@ use AssetPath, urls: ['/vendor/assets/javascripts'], root: File.expand_path('../
 use AssetPath, urls: ['/vendor/assets/javascripts'], root: rails_validations_path.full_gem_path
 
 DEFAULT_JQUERY_VERSION = '3.3.1'.freeze
-QUNIT_VERSION          = '2.6.1'.freeze
+QUNIT_VERSION          = '2.6.2'.freeze
 
 helpers do
   def jquery_version
