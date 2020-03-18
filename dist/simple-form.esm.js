063bb0faf53d87585af29085655d31b4639d8123
Geremia Taglialatela <tagliala.dev@gmail.com>
2020-03-18 20:33:09 +0100

    Regenerate assets

--- dist/simple-form.esm.js
+++ dist/simple-form.esm.js
@@ -1,6 +1,6 @@
 /*!
  * Client Side Validations Simple Form JS (Default) - v0.1.1 (https://github.com/DavyJonesLocker/client_side_validations-simple_form)
- * Copyright (c) 2019 Geremia Taglialatela, Brian Cardarella
+ * Copyright (c) 2020 Geremia Taglialatela, Brian Cardarella
  * Licensed under MIT (https://opensource.org/licenses/mit-license.php)
  */
 
