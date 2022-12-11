920e83da35a8311d9f21ceac70d7769eafc3b441
Geremia Taglialatela <tagliala.dev@gmail.com>
2022-12-11 11:00:22 +0100

    Update development dependencies
    Also fixes new RuboCop offences

--- lib/client_side_validations/simple_form.rb
+++ lib/client_side_validations/simple_form.rb
@@ -4,7 +4,7 @@ require 'simple_form'
 require 'client_side_validations'
 require 'client_side_validations/simple_form/form_builder'
 
-if defined?(::Rails)
+if defined?(Rails)
   require 'client_side_validations/simple_form/engine'
   require 'client_side_validations/generators/simple_form'
 end
