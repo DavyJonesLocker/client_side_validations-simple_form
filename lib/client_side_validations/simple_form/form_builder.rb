3848757f10b6e896505716c073217b12362fac1a
Geremia Taglialatela <tagliala.dev@gmail.com>
2023-08-27 16:38:31 +0200

    Minor improvements to form builder
    - Use `name` instead of `to_s` (name is 1.66x faster and does not
      allocate memory)
    - Memoize wrapper error component

--- lib/client_side_validations/simple_form/form_builder.rb
+++ lib/client_side_validations/simple_form/form_builder.rb
@@ -5,7 +5,7 @@ module ClientSideValidations
     module FormBuilder
       def client_side_form_settings(options, _form_helper)
         {
-          type:                self.class.to_s,
+          type:                self.class.name,
           error_class:         wrapper_error_component.defaults[:class].join(' '),
           error_tag:           wrapper_error_component.defaults[:tag],
           wrapper_error_class: wrapper.defaults[:error_class],
@@ -28,11 +28,12 @@ module ClientSideValidations
       private
 
       def wrapper_error_component
-        if namespace_present?(wrapper, :error)
-          wrapper.find(:error)
-        else
-          wrapper.find(:full_error)
-        end
+        @wrapper_error_component ||=
+          if namespace_present?(wrapper, :error)
+            wrapper.find(:error)
+          else
+            wrapper.find(:full_error)
+          end
       end
 
       def namespace_present?(component, namespace)
